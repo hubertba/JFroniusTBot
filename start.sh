43a820c9ea383a34f72caa5025903df803aeabc1
Hubert Baumgartner <hubert@chilicode.com>
2021-12-09 11:09:54 +0100

    fix date

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup java -jar target/JFroniusTBot-0.0.1-SNAPSHOT.jar > JFroniusTBot.$(date -d "today" +"%Y%m%d-%H:%M").log 2>&1 &
+nohup java -jar target/JFroniusTBot-0.0.1-SNAPSHOT.jar > JFroniusTBot.$(date '+%Y-%m-%d-%H-%M').log 2>&1 &
 echo $! > jFroniusTBot.pid.file

2f9b835cf15f49a326484cbc71f23536d7fc7cb2
Hubert Baumgartner <hubert@chilicode.com>
2021-12-09 09:53:27 +0100

    initial mvn skeleton

---
+++ src/main/java/com/chilicode/JFroniusTBot/JFroniusTBotApplication.java
@@ -0,0 +1,13 @@
+package com.chilicode.JFroniusTBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JFroniusTBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JFroniusTBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chilicode/JFroniusTBot/JFroniusTBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chilicode.JFroniusTBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JFroniusTBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
